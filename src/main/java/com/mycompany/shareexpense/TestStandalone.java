b705ade44dde4cc4d6c7522568e0323365203d5b
Muthukumaran Swaminathan <muthumech23@gmail.com>
2015-02-27 15:46:02 -0600

    issue fix

--- src/main/java/com/mycompany/shareexpense/TestStandalone.java
+++ src/main/java/com/mycompany/shareexpense/TestStandalone.java
@@ -1,5 +1,11 @@
 package com.mycompany.shareexpense;
 
+import org.jasypt.contrib.org.apache.commons.codec_1_3.binary.Base64;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -29,6 +35,20 @@ public class TestStandalone {
 
             System.out.println(Calendar.getInstance().get(Calendar.MONTH)+1);
 
+        String fileName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+
+        String signature = "";
+        byte dearr[] = Base64.decodeBase64(signature.getBytes());
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(new File("C:\\AndroidApp\\signature_"+ fileName +".png"));
+            fos.write(dearr);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
